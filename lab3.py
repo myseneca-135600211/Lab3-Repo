100fddace0c80e3e80431b3503e404a583b7ead4
Gitesh <113646387+myseneca-135600211@users.noreply.github.com>
2022-10-16 16:35:12 -0400

    Add files via upload

---
+++ lab3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def write_name_to_file():
+    f = open("name.txt", "w")
+    f.write("Gitesh Sethi")
+    f.close()
+
+if __name__ == "__main__":
+    write_name_to_file()
